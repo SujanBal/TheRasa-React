4fcd3f4fb7b3d85b08d8ab45ff8f4988aaac9cf7
Sujan <sujanbal001@gmail.com>
2021-03-28 17:24:19 +0545

    Fixed react scrolls

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -10,22 +10,22 @@ export default function Navbar() {
 				<Link className='navbar-logo' to='hero' activeClass="active" spy={true} smooth={true} offset={0} duration={500}><img src="images/logo-rosa.png" alt="logo-rosa" /></Link>
 				<ul className={openMenu ? "navbar-menu open" : 'navbar-menu'}>
 					<li className="navbar-menu-item">
-						<Link to='hero' activeClass="active" spy={true} smooth={true} offset={-105} duration={500} onClick={()=>setopenMenu(!openMenu)}>Home</Link>
+						<Link to='hero' activeClass="active" spy={true} smooth={true} offset={-135} duration={500} onClick={()=>setopenMenu(!openMenu)}>Home</Link>
 					</li>
 					<li className="navbar-menu-item">
-						<Link to='about' activeClass="active" spy={true} smooth={true} offset={-105} duration={500} onClick={()=>setopenMenu(!openMenu)}>About</Link>
+						<Link to='about' activeClass="active" spy={true} smooth={true} offset={-135} duration={500} onClick={()=>setopenMenu(!openMenu)}>About</Link>
 					</li>
 					<li className="navbar-menu-item">
-						<Link to='menu' activeClass="active" spy={true} smooth={true} offset={-105} duration={500} onClick={()=>setopenMenu(!openMenu)}>Menu</Link>
+						<Link to='menu' activeClass="active" spy={true} smooth={true} offset={-135} duration={500} onClick={()=>setopenMenu(!openMenu)}>Menu</Link>
 					</li>
 					<li className="navbar-menu-item">
-						<Link to='recipes' activeClass="active" spy={true} smooth={true} offset={-105} duration={500} onClick={()=>setopenMenu(!openMenu)}>Recipes</Link>
+						<Link to='recipes' activeClass="active" spy={true} smooth={true} offset={-135} duration={500} onClick={()=>setopenMenu(!openMenu)}>Recipes</Link>
 					</li>
 					<li className="navbar-menu-item">
-						<Link to='culinary' activeClass="active" spy={true} smooth={true} offset={-135} duration={500} onClick={()=>setopenMenu(!openMenu)}>Reservations</Link>
+						<Link to='culinary' activeClass="active" spy={true} smooth={true} offset={-185} duration={500} onClick={()=>setopenMenu(!openMenu)}>Reservations</Link>
 					</li>
 					<li className="navbar-menu-item">
-						<Link to='contact' activeClass="active" spy={true} smooth={true} offset={-175} duration={500} onClick={()=>setopenMenu(!openMenu)}>Contact</Link>
+						<Link to='contact' activeClass="active" spy={true} smooth={true} offset={-210} duration={500} onClick={()=>setopenMenu(!openMenu)}>Contact</Link>
 					</li>
 				</ul>
 				<i className={openMenu ? 'fas fa-times' : 'fas fa-bars'} onClick={()=>setopenMenu(!openMenu)}/>
